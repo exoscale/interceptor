2a858b93cc937030c3aee3b09756f23095bf03dd
Max Penet <max.penet@exoscale.ch>
2019-11-04 13:54:01 +0100

    Add license info

--- README.md
+++ README.md
@@ -188,18 +188,3 @@ something you desire (that's doable with context too).
 ## Installation
 
 [![Clojars Project](https://img.shields.io/clojars/v/exoscale/interceptor.svg)](https://clojars.org/interceptoroscale/interceptor)
-
-## License
-
-Copyright © 2019 exoscale
-
-This program and the accompanying materials are made available under the
-terms of the Eclipse Public License 2.0 which is available at
-http://www.eclipse.org/legal/epl-2.0.
-
-This Source Code may also be made available under the following Secondary
-Licenses when the conditions for such availability set forth in the Eclipse
-Public License, v. 2.0 are satisfied: GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or (at your
-option) any later version, with the GNU Classpath Exception which is available
-at https://www.gnu.org/software/classpath/license.html.
--- project.clj
+++ project.clj
@@ -1,13 +1,19 @@
 (defproject exoscale/interceptor "0.1.0-SNAPSHOT"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "ISC"}
   :url "https://github.com/exoscale/interceptor"
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :profiles {:dev
              {:dependencies [[org.clojure/clojurescript "1.10.439"]
                              [manifold "0.1.8"]
                              [org.clojure/core.async "0.4.500"]
-                             [cc.qbits/auspex "0.1.0-alpha2"]]}}
+                             [cc.qbits/auspex "0.1.0-alpha2"]]
+              :plugins [[lein-cljsbuild "1.1.7"]]
+
+              :cljsbuild {:builds
+                          [{:id "default"
+                            :source-paths ["src"]
+                            :compiler {:optimizations :whitespace
+                                       :pretty-print true}}]}}}
   :pedantic? :warn
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
@@ -16,4 +22,4 @@
                   ["deploy" "clojars"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
-                  ["vcs" "push"]]
+                  ["vcs" "push"]])
