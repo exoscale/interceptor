87f9d57f212e909d08681ce40cd8dc4648ca792e
Max Penet <max.penet@exoscale.ch>
2019-11-27 14:11:01 +0100

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/interceptor "0.1.6"
+(defproject exoscale/interceptor "0.1.7-SNAPSHOT"
   :license {:name "ISC"}
   :url "https://github.com/exoscale/interceptor"
   :dependencies [[org.clojure/clojure "1.10.1"]]
