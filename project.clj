a127c2642053ea627237e81f7c4e50bebaa3cb5d
Miguel Ping <miguel.ping@gmail.com>
2020-02-20 18:09:26 +0000

    Version 0.1.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/interceptor "0.1.8"
+(defproject exoscale/interceptor "0.1.9-SNAPSHOT"
   :license {:name "ISC"}
   :url "https://github.com/exoscale/interceptor"
   :dependencies [[org.clojure/clojure "1.10.1"]]
